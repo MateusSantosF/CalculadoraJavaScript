075d8505397c4368390aab98b859b878fc62fbf1
Mateus Santos <mateus.ferreira1@aluno.ifsp.edu.br>
2021-03-16 18:39:31 -0300

    Primeira versão Calculadora Básica

---
+++ indexJs.js
@@ -0,0 +1,14 @@
+
+var numb;
+function calcular(valor){
+    document.getElementById('display').value += valor;
+    numb = document.getElementById('display').value;    
+}
+
+function limpar(){
+    document.getElementById('display').value = '';
+}
+function resultado(){
+    //eval = Função nativa que interpreta uma string como uma função javascript
+    document.getElementById('display').value = eval(numb);
+}
